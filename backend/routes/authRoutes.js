d3128171eb7bd28cee5825665a3be504e0f05fa8
suman679 <sumanj8084@gmail.com>
2026-05-26 11:25:56 +0530

    Add auth validation

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,27 @@
+const { body } = require('express-validator');
 const express = require('express');
 const { register, login, getMe, updateProfile, updatePassword, logout } = require('../controllers/authController');
 const { protect } = require('../middleware/auth');
 const router = express.Router();
 
-router.post('/register',        register);
-router.post('/login',           login);
+router.post(
+  '/register',
+  [
+    body('name').trim().escape(),
+    body('email').isEmail().normalizeEmail(),
+    body('password').isLength({ min: 6 })
+  ],
+  register
+);
+
+router.post(
+  '/login',
+  [
+    body('email').isEmail().normalizeEmail(),
+    body('password').notEmpty()
+  ],
+  login
+);
 router.get('/me',               protect, getMe);
 router.put('/updateprofile',    protect, updateProfile);
 router.put('/updatepassword',   protect, updatePassword);
